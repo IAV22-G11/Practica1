82b17a807c3861ea228ca4fab0fc190213c4e4ca
mihern07 <mihern07@ucm.es>
2022-02-07 18:33:27 +0100

    Llegada del perro versión beta

---
+++ Assets/Scripts/Llegar.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCM.IAV.Movimiento
+{
+
+    /// <summary>
+    /// Clase para modelar el comportamiento de SEGUIR a otro agente
+    /// </summary>
+    public class Llegar : ComportamientoAgente
+    {
+        float radius = 1;
+        float timeToTarget = 0.25f;
+
+        /// <summary>
+        /// Obtiene la dirección
+        /// </summary>
+        /// <returns></returns>
+        public override Direccion GetDireccion()
+        {
+            Direccion result = new Direccion();
+            result.lineal = objetivo.transform.position - transform.position;
+
+            //Si ya hemos llegado se para de mover
+            if (result.lineal.magnitude < radius)
+            {
+                result.lineal = new Vector3();
+            }
+            else
+            {
+                //Para llegar al objetivo en timeTotarget segundos
+                result.lineal /= timeToTarget;
+
+                //Comprobamos que no pase de la velocidad máxima
+                if (result.lineal.magnitude > agente.velocidadMax)
+                {
+                    result.lineal.Normalize();
+                    result.lineal *= agente.velocidadMax;
+                }
+            }
+
+            return result;
+        }
+    }
+}
