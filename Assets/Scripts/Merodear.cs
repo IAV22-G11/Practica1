c230156349bef4725618c109cd426b7aa1abdb2f
Samu <Samuebla@ucm.es>
2022-02-15 12:26:53 +0100

    Prototipo Rata y Perro bien hecho
    El perro te sigue correctamente y las ratas se mueven correctamente (salvo cuando se chocan con la pared)
    
    Co-authored-by: mihern07 <mihern07@ucm.es>

--- Assets/Scripts/Merodear.cs
+++ Assets/Scripts/Merodear.cs
@@ -8,27 +8,75 @@ namespace UCM.IAV.Movimiento
     /// </summary>
     public class Merodear : ComportamientoAgente
     {
+        //Temporizador para que gire cada 1/5s
+        private float temp = 0;
+        private float tempChoque = -10;
+
+        private float rotation;
+
+        //Cambiamos nuestra orientacion aleatoriamente
+        int numAleatorio;
+
         /// <summary>
         /// Obtiene la dirección
         /// </summary>
         /// <returns></returns>
         public override Direccion GetDireccion()
         {
+
             Direccion result = new Direccion();
 
             //Sacamos la velocidad a partir de la forma vectorial de la orientacion
-            result.lineal = agente.velocidadMax * agente.OriToVec(agente.orientacion);
+            result.lineal = agente.velocidadMax * transform.forward;
 
-            //Cambiamos nuestra orientacion aleatoriamente
-            int numAleatorio;
-            if (Random.value < 0.5)
+
+
+
+            //Si todavia seguimos contando
+            if (temp > 0)
             {
-                numAleatorio = -1;
-            } else
-                numAleatorio = 1;
-            result.angular = numAleatorio * agente.rotacionMax;
+                temp -= Time.time;
+            }
+            //Rotamos al raton
+            else
+            {
+
+
+                if (Random.value < 0.5)
+                {
+                    numAleatorio = -1;
+                }
+                else
+                    numAleatorio = 1;
+
+                rotation = numAleatorio * agente.rotacionMax * Random.value;
+
+                //Ponemos un random entre 1 y 5 a temp
+                temp = Random.Range(200, 1000);
+            }
+
+            //Con random value hacemos que en vez de rotar X o -X rote tambien en los valores intermedios
+            result.angular = rotation;
+
+            tempChoque -= Time.time;
 
             return result;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (tempChoque < 0)
+            {
+                //Si el objeto con el que nos hemos chocado tiene la layer de pared...
+                if (other.gameObject.layer == 3)
+                {
+                    //Damos 180º a el raton
+                    transform.localEulerAngles = new Vector3(transform.rotation.x, transform.rotation.y +180, transform.rotation.z); // Flipped
+                    tempChoque = 2000;
+                }
+            }
+            
+
         }
     }
 }
